033acc0be29ef392c9182b86dcd00863aba9d772
omurilolima <msl@cesar.school>
2025-05-13 15:36:03 +0100

    Add a basic route

--- index.ts
+++ index.ts
@@ -6,4 +6,9 @@
 
 import express from "express";
 const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
 app.listen(8000, () => console.log("Server started+++"));
