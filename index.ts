383136f6a47cca58fe034c3ae86d002855e00b0b
omurilolima <msl@cesar.school>
2025-05-13 14:42:04 +0100

    Node + Typescript

---
+++ index.ts
@@ -0,0 +1,8 @@
+// 1. Run "npm init -y" to generate a basic package.json file
+// 2. Install ts-node running "npm i -D ts-node typescript"
+// 3. Go to package.json and add a new script: "start" : "node --loader ts-node/esm index.ts"
+// 4. Create a tsconfig.json file running: "npx tsc --init"
+// Run "npm start" to see the output of your program
+
+let x: number = 1;
+console.log(x);
