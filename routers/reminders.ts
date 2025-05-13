a0e013abfbb56a3e3a75c780d7deb35e57e8a29a
omurilolima <msl@cesar.school>
2025-05-13 16:33:30 +0100

    Building an API

--- routers/reminders.ts
+++ routers/reminders.ts
@@ -1,16 +1,22 @@
 import { Router } from "express";
 import CreateReminderDto from "../dtos/create-reminder";
+import Reminder from "../models/reminder";
 
 const router = Router();
+const reminders: Reminder[] = [];
 
+// GET the list of reminders
 router.get("/", (req, res) => {
-	res.send("List of reminders");
+	res.json(reminders);
 });
 
+// POST a new reminder
 router.post("/", (req, res) => {
 	const { title } = req.body as CreateReminderDto;
 	// send the title back to the client
-	res.json("title");
+	const reminder = new Reminder(title);
+	reminders.push(reminder);
+	res.status(201).json(reminder);
 });
 
 export default router;
